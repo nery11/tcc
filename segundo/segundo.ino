8ed1249e1162086b8012b6e711fba60c62b780c7
iury <iurynerygn@hotmail.com>
2019-10-25 13:53:48 -0300

    tcc pronto

--- segundo/segundo.ino
+++ segundo/segundo.ino
@@ -2,17 +2,17 @@
 
 #include <ESP8266WiFi.h>
 
-const char* ssid     = "CLICKNET_05";
-const char* password = "ap506toshiba";
+const char* ssid     = "NOME_REDE";
+const char* password = "SENHA_REDE";
 
-WiFiServer server(80);
+WiFiServer server(80);  //porta do roteador
 
 String header;
 
-const int output1 = D1;
-const int output2 = D2;
-const int output3 = D3;
-const int output4 = D4;
+const int output1 = D0;
+const int output2 = D1;
+const int output3 = D2;
+const int output4 = D3;
 
 String output1State = "off";
 String output2State = "off";
@@ -35,7 +35,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
+  // Imprima o endereço IP local e inicie o servidor web
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
@@ -45,23 +45,24 @@ void setup() {
 }
 
 void loop() {
-  WiFiClient client = server.available();   // Listen for incoming clients
+  WiFiClient client = server.available();   // Ouça os clientes recebidos
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+  if (client) {                             // Se um novo cliente se conectar,
+    Serial.println("New Client.");          // imprimir uma mensagem na porta serial
+    String currentLine = "";                // faça uma String para armazenar dados recebidos do cliente
+    while (client.connected()) {            // enquanto o cliente está conectado
+      if (client.available()) {             // bytes para ler do cliente,
+        char c = client.read();             //leia um byte e depois
+        Serial.write(c);                    // imprima-o no monitor serial
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {                    // se o byte for um caractere de nova linha
+          // se a linha atual estiver em branco, você terá dois caracteres de nova linha seguidos.
+          // como é o fim da solicitação HTTP do cliente, envie uma resposta:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
+            // Os cabeçalhos HTTP sempre começam com um código de resposta (por exemplo, HTTP / 1.1 200 OK)
+            // e um tipo de conteúdo para que o cliente saiba o que está por vir e, em seguida, uma linha em branco:
+//            client.println("Pagina OK");
+            client.println("HTTP / 1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");    
             client.println();
@@ -70,13 +71,13 @@ void loop() {
              client.println("<html>");
             client.println("<read>");
                            
-             client.println("<title>P&aacutegina nery</title");
+             client.println("<title>P&aacutegina do TCC</title");
              
              client.println("</read>");
              client.println("<body background=https://heypeppers.com.br/blog/wp-content/uploads/2017/04/74950-robotica-educacional-entenda-o-que-e-e-quais-sao-os-ganhos-no-seu-aprendizado-1.jpg > ");     
              client.println("</br>");
              client.println("<body>");
-             client.println("<H1><CENTER> P&aacutegina Iury Nery </CENTER></H1>");
+             client.println("<H1><CENTER> P&aacutegina do TCC </CENTER></H1>");
              client.println("<H1><CENTER> Controle da sua casa <CENTER></H1>");
              client.println("<br>");
              client.println("</body>");
@@ -85,83 +86,84 @@ void loop() {
 
 
                
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
+            // liga e desliga os GPIOs
+            if (header.indexOf("GET /Sala/on") >= 0) {  //No local de sala pode ser mudado para o desejado 
               Serial.println("Sala on");
               output1State = "on";
               digitalWrite(output1, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
+            } else if (header.indexOf("GET /Sala/off") >= 0) {
               Serial.println("Sala off");
               output1State = "off";
               digitalWrite(output1, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
+            } else if (header.indexOf("GET /Quarto/on") >= 0) {  //No local de Quarto pode ser mudado para o desejado
               Serial.println("Quarto on");
               output2State = "on";
               digitalWrite(output2, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
+            } else if (header.indexOf("GET /Quarto/off") >= 0) {
               Serial.println("Quarto off");
               output2State = "off";
               digitalWrite(output2, LOW);
             }
-             else if (header.indexOf("GET /3/on") >= 0) {
+             else if (header.indexOf("GET /Luz da garagem/on") >= 0) {  //No local de Luz da garagem pode ser mudado para o desejado
               Serial.println("Luz da garagem on");
               output3State = "on";
               digitalWrite(output3, HIGH);
-            } else if (header.indexOf("GET /3/off") >= 0) {
+            } else if (header.indexOf("GET /Luz da garagem/off") >= 0) {
               Serial.println("Luz de garagem off");
               output3State = "off";
               digitalWrite(output3, LOW);
             }
-               else if (header.indexOf("GET /2/on") >= 0) {
-              Serial.println("tomada ligado");
+               else if (header.indexOf("GET /Tomada ligada/on") >= 0) { //No local de tomada ligada pode ser mudado para o desejado
+              Serial.println("tomada ligada");
               output4State = "on";
               digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /2/off") >= 0) {
-              Serial.println("tomado desligado");
+            } else if (header.indexOf("GET /Tomada desligada/off") >= 0) {
+              Serial.println("tomada desligada");
               output4State = "off";
               digitalWrite(output4, LOW);
             }
             
-            // Display the HTML web page
+            // Exibir a página da web em HTML
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
+            // CSS para estilizar os botões liga / desliga
+            // Sinta-se à vontade para alterar os atributos de cor de fundo e tamanho da fonte para atender às suas preferências
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #77878A;}</style></head>");
             
             
-            // Display current state, and ON/OFF buttons for GPIO 5  
+            // Exibir estado atual e botões ON / OFF para GPIO D0 
             client.println("<B><p>Sala - State " + output1State + "</p></B>");
-            // If the output5State is off, it displays the ON button       
+            // Se o output5State estiver desativado, ele exibirá o botão ON      
             if (output1State=="off") {
               client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
             } 
                
-            // Display current state, and ON/OFF buttons for GPIO 4  
+            // Exibir estado atual e botões ON / OFF para GPIO  D1  
             client.println("<B><p>Quarto - State " + output2State + "</p></B>");
-            // If the output4State is off, it displays the ON button       
+            // Se o output4State estiver desativado, ele exibirá o botão ON       
             if (output2State=="off") {
               client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
+            //  Exibir estado atual e botões ON / OFF para GPIO D2
              client.println("<B><p>Luz da garagem - State " + output3State + "</p></B>");
-            // If the output5State is off, it displays the ON button       
+            // Se o output3State estiver desativado, ele exibirá o botão ON / OFF PARA GPIO        
             if (output3State=="off") {
               client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
             } 
                
-            // Display current state, and ON/OFF buttons for GPIO 4  
+            //  Exibir estado atual e botões ON / OFF para GPIO D3  
             client.println("<B><p>Tomado - State " + output4State + "</p></B>");
-            // If the output4State is off, it displays the ON button       
+            //  Se o output2State estiver desativado, ele exibirá o botão ON       
             if (output4State=="off") {
               client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
             } else {
@@ -173,21 +175,21 @@ void loop() {
             client.println("</body></html>");
            
              
-            // The HTTP response ends with another blank line
+            // A resposta HTTP termina com outra linha em branco
             client.println();
-            // Break out of the while loop
+            // Quebre o loop while
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else { // se você recebeu uma nova linha, limpe currentLine
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {  // se você tiver mais alguma coisa além de um caractere de retorno de caracter,
+          currentLine += c;      // adicione-o ao final do currentLine
         }
       }
     }
-    // Clear the header variable
+    // Limpe a variável de cabeçalho
     header = "";
-    // Close the connection
+    // Feche a conexão
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
